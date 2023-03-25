9f2c68e0e169bf3d2903f0d83874a260850e01bd
Vicente Antonio G. Reyes <icenreyes@icloud.com>
2023-03-25 13:51:13 +0800

    added rss feed

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import post_list, post_detail, post_share
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
     path('<int:post_id>/share/', post_share, name='post_share'),
     path('tag/<slug:tag_slug>/', post_list, name='post_list_by_tag'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
